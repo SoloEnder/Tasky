ab71e5baac6dfbbcea5fa8e1ab4defb78e583486
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-16 19:05:09 +0100

    add decorator for file operation exceptions

--- app/src/save_manager.py
+++ app/src/save_manager.py
@@ -5,39 +5,48 @@ from app.utils.username_hidder import remove_username
 
 logger = logging.getLogger(__name__)
 
-def save_data(filepath: str, data):
+def file_operation_exception(func):
 
+    def wrapper(filepath, data=None):
+        filepath_without_username = remove_username(filepath)
+
+        try:
+            func(filepath, data)
+
+        except FileNotFoundError:
+            logger.warning(f"No file found at {filepath_without_username}")
+            return
+        
+        except json.JSONDecodeError:
+            logger.warning(f"Unable to load data from file {filepath_without_username} : file corrupted")
+            return
+
+        except PermissionError:
+            logger.warning(f"Unable to load data from file {filepath_without_username} : permission denied")
+            return
+
+        except Exception as e:
+            logger.warning(f"Unable to load data from file {filepath_without_username}\n{e}")
+            return
+
+    return wrapper
+
+@file_operation_exception
+def save_data(filepath: str, data):
+    save_data.filepath = filepath
 
     with open(filepath, "w") as f:
         json.dump(data, f)
 
     logger.info(f"Data saved at {remove_username(filepath)}")
 
-def load_data(filepath: str):
-    filepath_without_username = remove_username(filepath)
+@file_operation_exception
+def load_data(filepath: str, data):
+    load_data.filepath = filepath
 
-    try:
+    with open(filepath, "r") as f:
+        data = json.load(f)
 
-        with open(filepath, "r") as f:
-            data = json.load(f)
-
-    except FileNotFoundError:
-        logger.warning(f"No file found at {filepath_without_username}")
-        return
-    
-    except json.JSONDecodeError:
-        logger.warning(f"Unable to load data from file {filepath_without_username} : file corrupted")
-        return
-
-    except PermissionError:
-        logger.warning(f"Unable to load data from file {filepath_without_username} : permission denied")
-        return
-
-    except Exception as e:
-        logger.warning(f"Unable to load data from file {filepath_without_username}\n{e}")
-        return
-
-    else:
-        logger.info(f"Data loaded from {remove_username(filepath)}")
-        return data
+    logger.info(f"Data loaded from {remove_username(filepath)}")
+    return data
         
