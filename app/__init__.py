1c55c9d6c2964bbb5bc28fc33b6035953ff278cc
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-16 18:32:34 +0100

    update logs file max size
    Update logs file max size to 1Mo

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import logging.handlers
 from app.utils import paths
 
 logger = logging.getLogger()
-logsfile_handler = logging.handlers.RotatingFileHandler(paths.logs_basefile, maxBytes=100000, backupCount=3)
+logsfile_handler = logging.handlers.RotatingFileHandler(paths.logs_basefile, maxBytes=1000000, backupCount=3)
 logsfile_handler.setLevel("DEBUG")
 logs_formatter = logging.Formatter(fmt="[{asctime}] - [{name}] - [{levelname}] : {msg}", style="{")
 logsfile_handler.setFormatter(logs_formatter)
