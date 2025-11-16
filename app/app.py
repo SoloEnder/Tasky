c02e601db125ea045be490ad23b3b3491c41b022
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-16 20:37:47 +0100

    update global logger location

--- app/app.py
+++ app/app.py
@@ -12,7 +12,9 @@ class App:
 
     def __init__(self):
         self.user = User(self)
-        check_and_make("program_dir", "data_dir", "user_data_dir", "logs_dir")
+        check_and_make("program_dir", "logs_dir")
+        self.set_logger()
+        check_and_make("data_dir", "user_data_dir")
         self.logger = logging.getLogger(__name__)
         self.tasks_data_handler = tasks_data_handler
         self.tasks_data_handler.load_tasks_data()
@@ -35,4 +37,17 @@ class App:
         self.logger.info("Auto saving...")
         self.tasks_data_handler.save_tasks_data()
         self.main_window.after(120000, self.auto_save)
+
+    def set_logger(self):
+        global_logger = logging.getLogger("app")
+        global_logsfile_handler = logging.handlers.RotatingFileHandler(paths.logs_basefile, maxBytes=1000000, backupCount=3)
+        global_logsfile_handler.setLevel("DEBUG")
+        global_logs_formatter = logging.Formatter(fmt="[{asctime}] - [{name}] - [{levelname}] : {msg}", style="{")
+        global_logsfile_handler.setFormatter(global_logs_formatter)
+        global_logs_streamhandler = logging.StreamHandler()
+        global_logs_streamhandler.setFormatter(global_logs_formatter)
+        global_logs_streamhandler.setLevel(logging.CRITICAL)
+        global_logger.addHandler(global_logsfile_handler)
+        global_logger.setLevel(logging.DEBUG)
+
 
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import tkinter as tk
 import logging
 from app.app import App
 
-logger = logging.getLogger("app")
+
 app = App()
+logger = logging.getLogger(__name__)
 print("Welcome to Tasks Manager v0.1.0")
 logger.info("Init app...")
 app.running()
