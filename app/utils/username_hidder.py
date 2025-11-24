908dc3b46ddfeaa354732cda6b997d06c4ce3599
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-24 19:40:01 +0100

    bug fix

--- app/utils/username_hidder.py
+++ app/utils/username_hidder.py
@@ -2,6 +2,8 @@
 import os
 
 def remove_username(path: str):
+    path = path.lower()
     username = os.path.expanduser("~")
+    username = username.lower()
     path = path.replace(username, "~")
     return path
