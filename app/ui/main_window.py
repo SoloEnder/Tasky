ad87ca2f54040a5dfe9edd9261c89fea08ae2032
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-26 12:05:40 +0100

    added simple screen switching

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,6 +1,8 @@
 import logging 
 import customtkinter as ctk
-from .tabview import TabView
+from .sidebar import SideBarFrame
+from .settings_screen import settings_screen
+from app.ui.tasks_screen import tasks_screen
 from ..src.tasks.tasks_data_handler import tasks_data_handler
 import logging
 
@@ -15,20 +17,34 @@ class MainWindow(ctk.CTk):
         self.master = master
         super().__init__()
         self.title("Tasks Manager")
-        self.resizable(False, False)
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=5)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.user = user
         self.tasks_data_handler = tasks_data_handler
         self.tasks_data = self.tasks_data_handler.tasks_data
-        self.tabview = TabView(self, user)
-        self.tabview.grid(sticky="nsew")
+        self.screens = {"Tasks":tasks_screen.TasksScreen(self, self.user), "Settings":settings_screen.SettingsScreen(self)}
+        self.current_screen = ("Tasks", self.screens["Tasks"])
+        self.side_bar = SideBarFrame(self, user)
+        self.side_bar.grid(row=0, column=0, sticky="nsew")
+        self.switch_frame("Tasks", destroy=False)
         self.logger = logging.getLogger(__name__)
 
     def on_closing(self):
         self.logger.debug("Window closure request received")
         self.destroy()
         self.logger.info("Window closed")
+
+    def switch_frame(self, frame_name: str, destroy: bool=False):
+        self.frame_object = self.screens[frame_name]
+
+        if destroy:
+            self.current_screen[1].destroy()
+
+        self.current_screen = (frame_name, self.frame_object)
+        self.current_screen[1].grid(row=0, column=1, sticky="nsew")
 
 
 
