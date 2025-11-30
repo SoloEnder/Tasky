7bcf3981b103d36640c89e0cda6638cf7752d7bc
SoloEnder <houngbedjileonide@protonmail.com>
2025-11-30 20:47:45 +0100

    added sidebar

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,5 +1,7 @@
 import logging 
 import customtkinter as ctk
+from app.utils import events_handlers
+from app.src.events_handler import Event
 from .sidebar import SideBarFrame
 from .settings_screen import settings_screen
 from .stats_screen import stats_screen
@@ -14,7 +16,7 @@ class MainWindow(ctk.CTk):
     Inherit from customtkinter.CTk
     """
 
-    def __init__(self, master, user):
+    def __init__(self, master):
         self.master = master
         super().__init__()
         self.title("Tasks Manager")
@@ -23,15 +25,28 @@ class MainWindow(ctk.CTk):
         self.columnconfigure(1, weight=5)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        self.user = user
+        self.events_handler = events_handlers.main_events_handler
         self.tasks_data_handler = tasks_data_handler
         self.tasks_data = self.tasks_data_handler.tasks_data
         self.logger = logging.getLogger(__name__)
-        self.screens = {"Tasks":tasks_screen.TasksScreen(self, self.user), "Settings":settings_screen.SettingsScreen(self), "Stats":stats_screen.StatsScreen(self)}
+        self.screens = {"Tasks":tasks_screen.TasksScreen(self), "Settings":settings_screen.SettingsScreen(self), "Stats":stats_screen.StatsScreen(self)}
         self.current_screen = ("Tasks", self.screens["Tasks"])
-        self.side_bar = SideBarFrame(self, user)
+        self.side_bar = SideBarFrame(self)
         self.side_bar.grid(row=0, column=0, sticky="nsew")
         self.switch_frame("Tasks", destroy=False)
+
+        #  Creating events
+        self.events_infos = {
+            "Tasky.Ui.ScreenSwitched":{"ancient_name":None, "ancient_object":None, "new_name":None, "new_object":None}, 
+            "Tasky.Ui.ScreenAdded":{"name":None, "object":None}, 
+            "Tasky.Ui.ScreenDeleted":{"name":None},
+            "Tasky.Ui.WindowsClosed":{},
+            }
+        
+        for event_name, event_kw in self.events_infos.items():
+            event = Event(event_name, **event_kw)
+            self.events_handler.add_event(event)
+
 
     def on_closing(self):
         self.logger.debug("Window closure request received")
@@ -39,9 +54,10 @@ class MainWindow(ctk.CTk):
         self.logger.info("Window closed")
 
     def switch_frame(self, frame_name: str, destroy: bool=False, ungrid_current: bool=True):
+        ancient_screen_name, ancient_screen_obj = self.current_screen
 
         if ungrid_current:
-            self.current_screen[1].grid_forget()
+            ancient_screen_obj.grid_forget()
 
         self.frame_object = self.screens[frame_name]
         self.logger.debug(f"Frame name : {frame_name}")
@@ -53,6 +69,28 @@ class MainWindow(ctk.CTk):
         self.current_screen = (frame_name, self.frame_object)
         self.current_screen[1].grid(row=0, column=1, sticky="nsew")
         self.logger.debug(f"Current screen : {self.frame_object}")
+        self.events_handler.raise_event(
+            "Tasky.Ui.ScreenSwitched", 
+            ancient_name=ancient_screen_name, 
+            ancient_oject=ancient_screen_obj, 
+            new_name=self.current_screen[0],
+            new_object=self.current_screen[1]
+            )
+        self.side_bar.refresh() 
 
+    def add_screen(self, name: str, object):
+        self.screens[name] = object
+        self.events_handler.raise_event(
+            "Tasky.Ui.ScreenAdded",
+            name=name,
+            object= object,
+            )
+        self.side_bar.refresh() 
 
-
+    def remove_screen(self, name: str):
+        del self.screens[name]
+        self.events_handler.raise_event(
+            "Tasky.Ui.ScreenDeleted",
+            name=name,
+            )
+        self.side_bar.refresh() 
--- app/ui/sidebar.py
+++ app/ui/sidebar.py
@@ -7,18 +7,19 @@ class SideBarFrame(ctk.CTkFrame):
     An instance of customtkinter.CTkFrame, which handle all the screens of the app
     """
 
-    def __init__(self, master, user, **kwargs):
+    def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.columnconfigure(0, weight=1)
         self.master = master
         self.buttons = {}
-        
+
+    def refresh(self):
         index = 0
 
-        for screen_name, screen_object in self.master.screens.items():  #type: ignore
+        for screen_name, in self.master.screens.keys():  #type: ignore
             button = ctk.CTkButton(self, text=screen_name, height=35, font=("default", 15), command=lambda sn=list(self.master.screens.keys())[index]: self.master.switch_frame(sn)) #type: ignore
             button.grid(row=index, column=0, sticky="new", pady=10)
             self.buttons[screen_name] = button
             index += 1
 
-        print(self.buttons)
+
