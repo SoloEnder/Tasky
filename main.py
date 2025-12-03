fd15c9f7f02bfdf17918a91f5a2ee73884dd02ac
SoloEnder <houngbedjileonide@protonmail.com>
2025-12-03 19:07:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from app.app import App
 
 app = App()
 logger = logging.getLogger(__name__)
-print("Welcome to Tasks Manager v0.1.0")
+print("Welcome to Tasky v0.1.0")
 logger.info("Init app...")
 app.running()
 
