9aef8dac4560f886116715841ed38ba299b81b35
SoloEnder <houngbedjileonide@protonmail.com>
2025-12-17 14:18:32 +0100

    deleted unused files

--- app/src/tasks/task_creator.py
+++
@@ -1,9 +0,0 @@
-
-import logging
-from ...ui.tasks_screen.task_fr import TaskFrame
-
-logger = logging.getLogger(__name__)
-
-def create_task(master, task_data):
-    logger.info("A task frame creation request has been received")
-    return TaskFrame(master, task_data=task_data)
